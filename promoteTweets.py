77372d1d2965d11ce94bc4f3d9f89a9b0ca56486
Yves Xie <yxi389@gmail.com>
2013-05-24 17:16:45 -0500

    works

--- promoteTweets.py
+++ promoteTweets.py
@@ -15,13 +15,13 @@ import random
 #@@@@@@ TO USERS@@@@@@@@
 #
 #
-# only need to modify SAMPLE, DATA,MCMCsteps
+# only need to modify SAMPLE, DATA,MCMCsteps,RESULTS
 #
 #
 # assume all tweets from the same account and therefore their reach is assumeds to be equal.
 # sample data contains multiple tweets. each tweet can have different number of datapoints.
 SAMPLE= 1000 # REACH should be larger and any value_dimension values.
-MCMCsteps = 300
+MCMCsteps = 500
 DATA={ # cd1: cost_dimension_1, vd1: value_dimension_1
         "tweet1":[{'cd1':10,'vd1':12},
                   {'cd1':20,'vd1':42},
@@ -30,15 +30,17 @@ DATA={ # cd1: cost_dimension_1, vd1: value_dimension_1
         "tweet3":[{'cd1':102,'vd1':105}],
         "tweet4":[{'cd1':322,'vd1':150}] }
 METRIC = {"cost_dimension_1":"time_elapsed_since_creation","value_dimension_1":"num_of_retweets"} # FYI
-INIT_GAMMA={"shape":1.5,"scale":1.5} # initial Gamma distribution
-RESULTS = {}
+INIT_GAMMA={"shape":2.,"scale":2.} # initial Gamma distribution
+RESULTS = {"tweet1":0,
+           "tweet2":0,
+           "tweet3":0,
+           "tweet4":0}
 # for tweet1: x1,...,x12 <= 10, 10< x13,...,x42<=20,
 #             20 < x43,....x10000 < 200000
 # all xi's are iid Gamma. The goal is to estimate Gamma's parameters
 def drawGammaTest( shape, scale ):
     s = np.random.gamma(shape, scale, SAMPLE)
     for t in DATA.keys():
-        RESULTS[t] = 0
         for row in DATA[t]:
             cut_off = row['cd1']
             # count the samples, whose response time is below cut off
@@ -52,9 +54,11 @@ def drawGammaTest( shape, scale ):
     #return sum(RESULTS) # 
 def MCMCitr( steps ):
     for i in xrange(steps):
-        drawGammaTest(INIT_GAMMA['shape'],INIT_GAMMA['scale'])
-        INIT_GAMMA['shape'] += random.random()/3
-        INIT_GAMMA['scale'] += random.random()/3
-        print RESULTS
+        if (INIT_GAMMA['shape'] > 0 and INIT_GAMMA['scale'] > 0 ):
+            drawGammaTest(INIT_GAMMA['shape'],INIT_GAMMA['scale'])
+        INIT_GAMMA['shape'] += random.random() - 0.44
+        INIT_GAMMA['scale'] += random.random() - 0.44
+        if i%10 == 0:
+            print RESULTS
 
 MCMCitr(MCMCsteps)
