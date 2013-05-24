3b750d5706ceb0e14dfaa058c739ac274f9a5af8
Yves Xie <yxi389@gmail.com>
2013-05-24 15:49:26 -0500

    use Gamma instead so make the algo similar to MBT

--- promoteTweets.py
+++ promoteTweets.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
 # tweets promotion baseline algorithm
-# exponential parameter estimation
-# using exponential cdf
+# let us assume each user's retweet action is modeled by Gamma
+# and in the situation of lacking data, we for now assume users and homogeneous.
+# A simplified verions of patented MBT algorithm
 import sys
 import os
 import MySQLdb
@@ -11,9 +12,11 @@ import numpy as np
 import math
 # 1: promote, -1: not to promote, 0: it doesn't know what to do.
 
+# assume all tweets from the same account and therefore their reach is assumeds to be equal.
 # sample data contains multiple tweets. each tweet can have different number of datapoints.
-METRIC = {"cost_dimension_1":"time_elapsed","value_dimension_1":"retweets"} # FYI
-SAMPLE={
+METRIC = {"cost_dimension_1":"time_elapsed_since_creation","value_dimension_1":"num_of_retweets"} # FYI
+REACH = 10000 # REACH should be larger and any value_dimension values.
+SAMPLE={ # cd1: cost_dimension_1, vd1: value_dimension_1
         "tweet1":[{'cd1':10,'vd1':12},
                   {'cd1':20,'vd1':42},
                   {'cd1':60,'vd1':72}],
