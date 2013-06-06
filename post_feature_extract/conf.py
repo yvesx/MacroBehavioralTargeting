306c5ca7bfc60ed6af75d72e861f5028373de469
Yves Xie <yxi389@gmail.com>
2013-06-05 22:44:41 -0500

    post feature extract conf

---
+++ post_feature_extract/conf.py
@@ -0,0 +1,16 @@
+#!/home/download/python2.7/bin/python2.7
+
+
+class config:
+    def __init__(self):
+
+        self.debug        = True
+        self.log_critical = '/tmp/scclient_critical'
+        self.log_message  = '/tmp/scclient_message'
+        self.log_debug    = '/tmp/scclient_debug'
+
+        #self.dbpedia_api = "http://spotlight.dbpedia.org/rest/annotate/"
+        self.dbpedia_api = "http://10.1.1.129:9999/rest/annotate/"
+        self.wiki_api = "http://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&explaintext&exsectionformat=plain&titles="
+
+        self.java_trial = 1
