c881775b6de8bcc14a50264f4c18b82756d828be
Yves Xie <yxi389@gmail.com>
2013-06-08 21:09:37 -0500

    implement time_since_last_post

--- post_feature_extract/fire_requests.py
+++ post_feature_extract/fire_requests.py
@@ -58,9 +58,18 @@ def returnListOfFBIDs():
     listOfID[str(row[0])] =  {"fb_name":fb_name , "category":category}
   return listOfID
 
+def findLargestNegative(lst, num):
+  lst = [x - num for x in lst]
+  lst = [x for x in lst if x < 0]
+  lst.sort()
+  if len(lst) > 0:
+    return -lst[-1]
+  else:
+    return 0
 def getPosts():
-  listOfID = {}
-  query = "SELECT post_id, post_message , post_date, num_of_post_likes , num_of_comments , num_of_shares ,\
+  listOfPosts = []
+  unix_stamps = []
+  query = "SELECT post_id, post_message , UNIX_TIMESTAMP(post_date), num_of_post_likes , num_of_comments , num_of_shares ,\
                   post_type FROM %s.%s%s LIMIT 50" % (config.db , fb_id , config.suffix)
   try:
     cursor_s.execute(query)
@@ -70,16 +79,26 @@ def getPosts():
   for i in xrange(cursor_s.rowcount):
     row = cursor_s.fetchone()
     try:
-      fb_name = unicode(row[1], errors='ignore')
+      post_message = unicode(row[1], errors='ignore')
     except:
-      fb_name = u""
-    try:
-      category = unicode(row[2], errors='ignore')
-    except:
-      category = u""
-    listOfID[str(row[0])] =  {"fb_name":fb_name , "category":category}
+      post_message = u"unicode error"
+    post = { "fb_id": fb_id,
+             "post_id": row[0],
+             "text": post_message,
+             "unix_stamp": int(row[2]),
+             "num_of_post_likes": int(row[3]),
+             "num_of_comments": int(row[4]),
+             "num_of_shares": int(row[5]),
+             "post_type": row[6]
+            }
+    unix_stamps.add(int(row[2]))
+    listOfPosts.add(post)
 
-  return listOfID
+  # need to get the time_since_last_post variable.
+  unix_stamps.sort()
+  for post in listOfPosts:
+    post[""]
+  return listOfPosts
 
 listOfAllID=returnListOfFBIDs()
 try:
