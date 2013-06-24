0f3c83f4194a3dd406d89b3f0c6f4498cd243178
Yves Xie <yxi389@gmail.com>
2013-06-24 16:35:27 -0500

    update dense feature matrix

--- post_feature_extract/get_post_similarity.py
+++ post_feature_extract/get_post_similarity.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python
 # python get_post_similarity.py 309506851302 bmw_post_sim.json
 # python get_post_similarity.py put bmw_post_sim.json
+# python get_post_similarity.py scipy bmw_post_sim.json bmw_dense_feature.json
 
 import sys
 import os
-import MySQLdb
 import string
 import time
-import config
 import random
 #import pymongo
 #from pymongo import MongoClient
@@ -16,10 +15,6 @@ import urllib2
 import urllib
 import itertools
 import cPickle as pickle
-
-config = config.config()
-cursor_s = config.cursor_s
-
 
 
 def getPostsResponse(fbid):
@@ -75,15 +70,37 @@ def buildSimMatrix(PostsUsers):
                    "post1_size":len(PostsUsers[pair[0]]),
                    "post2_size":len(PostsUsers[pair[1]])})
   return matrix
+
+def buildNonSparseFeature(PostsUsers):
+    unique_user_ids = set()
+    array_to_scipy = []
+    for key in PostsUsers.keys():
+      unique_user_ids = unique_user_ids.union(PostsUsers[key])
+    for key in PostsUsers.keys():
+        access_set = PostsUsers[key]
+        mask = [ 1 if uid in access_set else 0 for uid in unique_user_ids ]
+        # only keeyp nonsparse ones.
+        array_to_scipy.append(mask)
+    return array_to_scipy
+
 def PutMongo(array):
   mongo_collection = config.mongo_matrix_collection
   mongo_collection.insert(array,continue_on_error=True)
   
 
 if (str(sys.argv[1]) == 'put'):
+  import config
+  config = config.config()
   m = buildSimMatrix(pickle.load(open(sys.argv[2],"rb")))
   PutMongo(m)
+elif (str(sys.argv[1]) == 'scipy'):
+  PU = buildNonSparseFeature(pickle.load(open(sys.argv[2],"rb")))
+  pickle.dump( PU, open( sys.argv[3], "wb" ) )
+  # bla
 else:
+  import config
+  config = config.config()
+  cursor_s = config.cursor_s
   fb_id = str(sys.argv[1])
   PU = getPostsResponse(fb_id)
   pickle.dump( PU, open( sys.argv[2], "wb" ) )
