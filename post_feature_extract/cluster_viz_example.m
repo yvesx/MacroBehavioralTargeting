047c94b2d5a62b5587b84d711ee8bc8c79552186
Yves Xie <yxi389@gmail.com>
2013-06-25 21:54:51 -0500

    adapt sample code for viz

---
+++ post_feature_extract/cluster_viz_example.m
@@ -0,0 +1,76 @@
+clear all
+close all
+load fisheriris
+load samsung_matlab_matrix
+labelx = 'Likes';
+labely = 'Viral';
+labelz = 'Rally';
+ftr1='SL';ftr2='SW';
+ftr3='PL';ftr4='PW';
+clstr1='NewProduct';clstr2='Discount';clstr3='Greeting';
+clstrs={clstr1, clstr2, clstr3};
+meas = double(dense);
+
+%% three clusters
+[cidx3,cmeans3,sumd3] = kmeans(meas,3,'replicates',5,'display','final');
+[silh3,h] = silhouette(meas,cidx3,'sqeuclidean');
+for i = 1:3
+    clust = find(cidx3==i);
+    plot3(meas(clust,1),meas(clust,5),meas(clust,21),ptsymb{i});
+    hold on
+end
+plot3(cmeans3(:,1),cmeans3(:,2),cmeans3(:,3),'ko');
+plot3(cmeans3(:,1),cmeans3(:,2),cmeans3(:,3),'kx');
+hold off
+xlabel(labelx); ylabel(labely); zlabel(labelz);
+view(-137,10);
+grid on
+figure
+
+%% three clusters with cosine distance
+[cidxCos,cmeansCos] = kmeans(meas,3,'dist','cos');
+[silhCos,h] = silhouette(meas,cidxCos,'cos');
+for i = 1:3
+    clust = find(cidxCos==i);
+    plot3(meas(clust,1),meas(clust,2),meas(clust,3),ptsymb{i});
+    hold on
+end
+hold off
+xlabel(labelx); ylabel(labely); zlabel(labelz);
+view(-137,10);
+grid on
+figure
+
+lnsymb = {'b-','r-','m-'};
+names = {ftr1,ftr2,ftr3,ftr4};
+meas0 = meas ./ repmat(sqrt(sum(meas.^2,2)),1,4);
+ymin = min(min(meas0));
+ymax = max(max(meas0));
+for i = 1:3
+    subplot(1,3,i); plot(meas0(cidxCos==i,:)',lnsymb{i});
+    hold on; plot(cmeansCos(i,:)','k-','LineWidth',2); hold off;
+    title(clstrs(i));
+    set(gca,'Xlim',[.9 4.1],'XTick',1:4,'XTickLabel',names,'YLim',[ymin ymax])
+end
+figure
+
+subplot(1,1,1);
+for i = 1:3
+    clust = find(cidxCos==i);
+    plot3(meas(clust,1),meas(clust,2),meas(clust,3),ptsymb{i});
+    hold on
+end
+xlabel(labelx); ylabel(labely); zlabel(labelz);
+view(-137,10);
+grid on
+sidx = grp2idx(species);
+miss = find(cidxCos ~= sidx);
+plot3(meas(miss,1),meas(miss,2),meas(miss,3),'k*');
+legend({clstr1,clstr2,clstr3});
+hold off
+figure
+
+eucD = pdist(meas,'cosine');
+clustTreeEuc = linkage(eucD,'average');
+[h,nodes] = dendrogram(clustTreeEuc,0);
+set(gca,'TickDir','out','TickLength',[.002 0],'XTickLabel',[]);
